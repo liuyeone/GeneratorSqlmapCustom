390fc2132c8fa08df0559f000beb24565835c7c9
liuye1 <liuye1@360.cn>
2019-05-14 17:27:26 +0800

    init

--- src/GeneratorSqlmap.java
+++ src/GeneratorSqlmap.java
@@ -1,13 +1,11 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
-import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GeneratorSqlmap {
 
@@ -22,7 +20,8 @@ public class GeneratorSqlmap {
 		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
 				callback, warnings);
 		myBatisGenerator.generate(null);
-	} 
+	}
+
 	public static void main(String[] args) throws Exception {
 		try {
 			GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
